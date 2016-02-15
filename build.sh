5db857336419dbbb4f4744903fb9fe47ec47314a
Max Goltzsche <max.goltzsche@algorythm.de>
2016-02-16 00:14:43 +0100

    vhost certificate changed. build.sh WEBDAV_DEPLOY_HOST variable introduced

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-HOST='192.168.122.209'
+if [ -z "$WEBDAV_DEPLOY_HOST" ]; then
+	WEBDAV_DEPLOY_HOST='192.168.122.209'
+fi
 BUILD_VERSION=$(cat pom.xml | grep -Po '(?<=<version>)([^<]+)' | head -n 1)
 
 if [ "$1" != 'deploy' ]; then
@@ -9,8 +11,11 @@ if [ "$1" != 'deploy' ]; then
 	fi
 fi
 
-rsync "target/webdav-client-$BUILD_VERSION-web.zip" $HOST:/home/max/webdav-client.zip &&
-ssh $HOST "cd /tmp && rm -rf webdav-client-$BUILD_VERSION; \
+rsync "target/webdav-client-$BUILD_VERSION-web.zip" $WEBDAV_DEPLOY_HOST:/home/max/webdav-client.zip &&
+ssh $WEBDAV_DEPLOY_HOST "cd /tmp && rm -rf webdav-client-$BUILD_VERSION; \
 	unzip /home/max/webdav-client.zip && \
 	rm -rf /var/www/robin/web/ui && \
-	mv webdav-client-$BUILD_VERSION /var/www/robin/web/ui"
+	mv webdav-client-$BUILD_VERSION /var/www/robin/web/ui && \
+	chown -R max:www-data /var/www/robin/web/ui && \
+	chmod -R 755 /var/www/robin/web/ui && \
+	find /var/www/robin/web/ui/ -type f | xargs chmod 644"
