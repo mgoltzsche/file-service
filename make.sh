3c15daee8bea2339460732d9967fd16482519504
Max Goltzsche <max.goltzsche@algorythm.de>
2017-06-26 01:43:34 +0200

    configured server image resolutions as within client

--- make.sh
+++ make.sh
@@ -1,11 +1,14 @@
 # Install docker: wget -qO- https://get.docker.com/ | sh
 
+IMAGE_NAME=mgoltzsche/file-service:latest
+# TODO: let JS read resolutions from server API
+RESOLUTIONS='-e IMAGE_CROP_27_23=true -e IMAGE_RESIZE_1898_1024=true -e IMAGE_RESIZE_1344_968=true -e IMAGE_RESIZE_618_584=true'
+
 buildJS() {
 	docker run -it --rm --name webdav-ui-build -v "$(pwd):/src" -w /src node:6.11.0 /bin/sh -c "npm install && npm run gulp"
 }
 
 buildImage() {
-	IMAGE_NAME=mgoltzsche/file-service:latest
 	docker build -t "$IMAGE_NAME" --rm .
 }
 
@@ -24,10 +27,10 @@ while [ $# -gt 0 ]; do
 			buildImage
 			;;
 		run)
-			docker run --rm --name webdav-service -e IMAGE_CROP_27_23=true -e IMAGE_RESIZE_100_100=true -p 80:80 "$IMAGE_NAME"
+			docker run --rm --name webdav-service $RESOLUTIONS -p 80:80 "$IMAGE_NAME"
 			;;
 		work)
-			docker run --rm --name webdav-service -p 80:80 -v "$(pwd)/dist:/var/www/html" "$IMAGE_NAME"
+			docker run --rm --name webdav-service $RESOLUTIONS -p 80:80 -v "$(pwd)/dist:/var/www/html" "$IMAGE_NAME"
 			;;
 		*)
 			echo "Usage: $0 [js|image|run|work]"
